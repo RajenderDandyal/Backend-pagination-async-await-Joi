fdc64a777ead33a793ea0ca5590b626ee7e6bf16
RajenderDandyal <39468888+RajenderDandyal@users.noreply.github.com>
2018-11-12 21:10:26 +0530

    completed

---
+++ src/api/resources/song/song.router.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/prefer-default-export */
+import express from 'express';
+import songController from './song.controller';
+
+export const songRouter = express.Router();
+songRouter
+  .route('/')
+  .post(songController.create)
+  .get(songController.findAll);
+
+songRouter
+  .route('/:id')
+  .get(songController.findOne)
+  .delete(songController.delete)
+  .put(songController.update);
